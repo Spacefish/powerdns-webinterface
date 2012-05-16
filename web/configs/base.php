88873f600100c545d85f5b9a48264bb79d9d30cf
Timo Witte <timo.witte@googlemail.com>
2012-05-17 00:01:10 +0200

    - changed version to 1.4.3

--- web/configs/base.php
+++ web/configs/base.php
@@ -7,7 +7,7 @@ $cfg['base'] = array(
 	"demomode" => false,
 	"locale" => "",	// leave blank for browserdetect,
 			// choose de_DE.utf8 if you want german or en_US.utf8 for english
-	"version" => "1.4.2"
+	"version" => "1.4.3"
 );
 
 ?>
