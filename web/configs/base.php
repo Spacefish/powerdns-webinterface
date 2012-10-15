72909f47bf4fce17b45c5df039b8c4f168fea18a
Timo Witte <timo.witte@googlemail.com>
2012-10-15 23:47:42 +0200

    - changes for packaging 1.5.1

--- web/configs/base.php
+++ web/configs/base.php
@@ -4,9 +4,9 @@
  * This configfile holds some basic configuration
  */
 $cfg['base'] = array(
-	"demomode" => true,
+	"demomode" => false,
 	"locale" => "",	// leave blank for browserdetect,
 			// choose de_DE.utf8 if you want german or en_US.utf8 for english
 	"default_timezone" => "Europe/Berlin",
-	"version" => "1.5"
+	"version" => "1.5.1"
 );
