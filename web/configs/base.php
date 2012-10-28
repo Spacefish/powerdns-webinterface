9aba3e62e4d29cf02c02cd571153178e04c9df7f
Timo Witte <timo.witte@gmail.com>
2012-10-28 02:20:02 +0100

    - bump version to 1.5.2

--- web/configs/base.php
+++ web/configs/base.php
@@ -8,5 +8,5 @@ $cfg['base'] = array(
 	"locale" => "",	// leave blank for browserdetect,
 			// choose de_DE.utf8 if you want german or en_US.utf8 for english
 	"default_timezone" => "Europe/Berlin",
-	"version" => "1.5.1"
+	"version" => "1.5.2"
 );
